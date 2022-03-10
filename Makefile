b67ee9ec94922dd2ad51bb1c69a9794ae55aab9c
MOOTEZSLIM <mootez.slim@esprit.tn>
2022-03-10 14:22:23 +0100

    supprimer

--- Makefile
+++
@@ -1,8 +0,0 @@
-Game:menu.o
-	gcc menu.o image.o text.o -o Game -lSDL -lSDL_image -lSDL_mixer -lSDL_ttf -g
-menu.o:menu.c
-	gcc -c menu.c -g
-image.o:image.c
-	gcc -c image.c -g
-text.o:text.c
-	gcc -c text.c -g
--- fichiers.c/text.c
+++
@@ -1,9 +0,0 @@
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
-#include "text.h"
-
-void displayText(text t,SDL_Surface *screen)
-{
-	t.surtext=TTF_RenderText_Solid(t.font,t.text,t.TColor);
-	SDL_BlitSurface(t.surtext,NULL,screen,&t.pos);
-}
